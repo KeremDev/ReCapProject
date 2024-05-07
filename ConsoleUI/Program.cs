5a4c5cf6e2c0614b60e46575a153ab1fae07bf44
Kerem Kayalar <kayalar.kerem@gmail.com>
2024-05-07 16:14:14 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,14 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        CarManager carmanager = new CarManager(new InMemoryCarDal());
+        foreach (var car in carmanager.GetAll())
+        {
+            Console.WriteLine(car.Description);
+        }
+    }
+}
